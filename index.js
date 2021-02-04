88c863ee63ad7093431f1b016d33b561480a09e5
David Karolyi <davidkarolyi96@gmail.com>
2021-02-04 19:47:38 +0100

    Move all hands

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-const DEGS_IN_CIRCLE = 360;
-const SECS_IN_MIN = 60;
-const MS_IN_SEC = 1000;
+const MS_PER_SEC = 1000;
+const MS_PER_MIN = 60 * MS_PER_SEC;
+const MS_PER_HOUR = 60 * MS_PER_MIN;
+const MS_PER_TWELWE_HOURS = 12 * MS_PER_HOUR;
 
-
-const hourHand = document.querySelector(".hour-hand");
-const secondHand = document.querySelector(".second-hand");
+const hoursHand = document.querySelector(".hours-hand");
+const minutesHand = document.querySelector(".minutes-hand");
+const secondsHand = document.querySelector(".seconds-hand");
 
 function updateHands() {
   const date = new Date();
-  const millisecDegrees = date.getMilliseconds() * DEGS_IN_CIRCLE / (SECS_IN_MIN * MS_IN_SEC);
-  const secDegrees = date.getSeconds() * DEGS_IN_CIRCLE / SECS_IN_MIN;
 
-  const deg = secDegrees + millisecDegrees;
-  secondHand.style.transform = `rotate(${deg}deg)`;
+  const secondsInMs = date.getSeconds() * MS_PER_SEC + date.getMilliseconds();
+  const minutesInMs = date.getMinutes() * MS_PER_MIN + secondsInMs;
+  const hoursInMs = date.getHours() * MS_PER_HOUR + minutesInMs;
+
+  const secondsDegrees = secondsInMs / MS_PER_MIN * 360;
+  const minutesDegrees = minutesInMs / MS_PER_HOUR * 360
+  const hoursDegrees = hoursInMs / MS_PER_TWELWE_HOURS * 360;
+
+  secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
   requestAnimationFrame(updateHands);
 }
+
+
 
 requestAnimationFrame(updateHands);
